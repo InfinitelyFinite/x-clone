a862a1be171a5e0ba9dd9b9253eb7704f39c0b06
sujal <bhattaraisujal712@gmail.com>
2024-07-02 22:25:26 +0545

    completed UI design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,26 @@ import { Route, Routes } from "react-router-dom";
 import SignUpPage from "./pages/auth/signup/SignUpPage";
 import HomePage from "./pages/home/HomePage";
 import LoginPage from "./pages/auth/login/LoginPage";
+import NotificationPage from "./pages/notification/NotificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
+import Sidebar from "./components/common/Sidebar";
+import RightPanel from "./components/common/RightPanel";
 
 function App() {
 
 
   return (
     <div className='flex max-w-6xl mx-auto'>
+      {/* common component since it is not wrapped with routes */}
+      <Sidebar/>
       <Routes>
         <Route path="/" element={<HomePage/>}/>
         <Route path="/login" element={<LoginPage/>}/>
         <Route path="/signup" element={<SignUpPage/>}/>
+        <Route path="/notifications" element={<NotificationPage/>}/>
+        <Route path="/profile/:username" element={<ProfilePage/>}/>
       </Routes>
+      <RightPanel/>
     </div>
   )
 }
