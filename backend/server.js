43d8bb31c6a949ea03d2eaefac34f30c64ce538a
sujal <bhattaraisujal712@gmail.com>
2024-07-05 01:22:22 +0545

    prepared for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
-
+import path from "path";
 import authRoute from "./routes/auth.route.js";
 import userRoute from "./routes/user.route.js";
 import postRoute from "./routes/post.route.js";
@@ -10,15 +10,17 @@ import notificationRoute from "./routes/notification.route.js";
 
 import { v2 as cloudinary } from "cloudinary";
 
-const app = express();
-const PORT = process.env.PORT || 8000;
-
 dotenv.config();
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+const __dirname = path.resolve();
+
 // middleware for parsing req.body
 app.use(express.json({limit: "5mb"}));
 // limit shouldn't be too large to prevent Denial of Service attack
@@ -29,6 +31,12 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/notifications", notificationRoute);
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
