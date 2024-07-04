b9ff453ae0806c228d462104eabf00a8e38726c7
sujal <bhattaraisujal712@gmail.com>
2024-07-04 23:14:12 +0545

    user info and profile page posts/likes fetched

--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -3,7 +3,7 @@ import PostSkeleton from "../skeletons/PostSkeleton";
 import {useQuery} from "@tanstack/react-query";
 import { useEffect } from "react";
 
-const Posts = ({feedType}) => {
+const Posts = ({feedType, username, userId}) => {
 
 	const getPostEndpoint = () =>{
 		switch(feedType){
@@ -11,10 +11,14 @@ const Posts = ({feedType}) => {
 				return "/api/posts/all";
 			case "following":
 				return "/api/posts/following";
+			case "posts":
+				return `/api/posts/user/${username}`;
+			case "likes":
+				return `/api/posts/likes/${userId}`;
 			default:
 				return "/api/posts/all";
 		}
-	}
+	};
 	const POST_ENDPOINT =getPostEndpoint();
 	const {data: posts, isLoading, refetch, isRefetching} = useQuery({
 		queryKey: ["posts"],
@@ -33,7 +37,7 @@ const Posts = ({feedType}) => {
 	});
 	useEffect(()=>{
 		refetch();
-	}, [feedType, refetch]);
+	}, [feedType, refetch, username]);
 	return (
 		<>
 			{isLoading || isRefetching && (
